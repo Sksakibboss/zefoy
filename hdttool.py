e465241b7667f599b939fd34e2f4b7633832f39f
Sksakibboss <105601888+Sksakibboss@users.noreply.github.com>
2023-11-09 16:06:36 +0600

    Add files via upload

---
+++ hdttool.py
@@ -0,0 +1,355 @@
+import requests,os,sys, time
+from time import strftime
+from random import choice, randint, shuffle
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+from os.path import isfile
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re,requests,os,sys
+from time import sleep 
+from datetime import date
+import requests, random
+import requests
+import base64, json,os
+from datetime import date
+from datetime import datetime
+from time import sleep,strftime
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re,requests,os,sys
+from time import sleep 
+from datetime import date
+import requests, random
+import uuid, re
+from pystyle import Write,Colors
+from bs4 import BeautifulSoup
+import socket
+from datetime import datetime
+from rich.table import Table as me
+from multiprocessing.pool import ThreadPool
+from bs4 import BeautifulSoup as parser
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+from rich import print as kui
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+import calendar
+from time import sleep as jeda
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+import os,sys
+os.system('clear')
+#màu
+luc = "\033[1;32m"
+trang = "\033[1;37m"
+do = "\033[1;31m"
+vang = "\033[0;93m"
+hong = "\033[1;35m"
+xduong = "\033[1;34m"
+xnhac = "\033[1;36m"
+red='\u001b[31;1m'
+yellow='\u001b[33;1m'
+green='\u001b[32;1m'
+blue='\u001b[34;1m'
+tim='\033[1;35m'
+xanhlam='\033[1;36m'
+xam='\033[1;30m'
+black='\033[1;19m'
+#key tool
+now = datetime.now()
+thu = now.strftime("%A")
+ngay_hom_nay = now.strftime("%d")
+thang_nay = now.strftime("%m")
+nam_ = now.strftime("%Y")
+ #--------------------KEY---------------&&#
+import requests
+from time import strftime
+
+
+import threading,base64
+import os,time,re,json,random
+from datetime import datetime
+from time import sleep,strftime
+import requests
+import os, sys
+try:
+  from faker import Faker
+  from requests import session
+  from colorama import Fore, Style
+  import requests, random, re
+  from random import randint
+  import requests,pystyle
+except:
+  os.system("pip install faker")
+  os.system("pip install requests")
+  os.system("pip install colorama")
+  os.system('pip install requests && pip install bs4 && pip install pystyle')
+  print('__Vui Lòng Chạy Lại Tool__')
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+#MÀU
+xnhac = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+hong = "\033[1;35m"
+trang = "\033[1;39m"
+whiteb="\033[1;39m"
+red="\033[0;31m"
+redb="\033[1;31m"
+end='\033[0m'
+#ĐÁNH DẤU BẢN QUYỀN
+dev="\033[1;39m[\033[1;31m×\033[1;39m]\033[1;39m"
+thanh_xau=trang+'~'+red+'['+vang+'⟨⟩'+red+'] '+trang+'➩ '+luc
+thanh_dep=trang+'~'+red+'['+luc+'✓'+red+'] '+trang+'➩ '+luc
+def banner():
+ banner = f"""
+\033[1;34m╔═════════════════════════════════════════════════════════════════╗
+\033[1;32m║ ██╗  ██╗██████╗ ████████╗  ████████╗ ██████╗  ██████╗ ██╗       ║
+\033[1;35m║ ██║  ██║██╔══██╗╚══██╔══╝  ╚══██╔══╝██╔═══██╗██╔═══██╗██║       ║
+\033[1;31m║ ███████║██║  ██║   ██║ █████╗ ██║   ██║   ██║██║   ██║██║       ║
+\033[1;33m║ ██╔══██║██║  ██║   ██║ ╚════╝ ██║   ██║   ██║██║   ██║██║       ║
+\033[1;34m║ ██║  ██║██████╔╝   ██║        ██║   ╚██████╔╝╚██████╔╝███████╗  ║
+\033[1;37m║ ╚═╝  ╚═╝╚═════╝    ╚═╝        ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝  ║
+\033[1;34m╠═════════════════════════════════════════════════════════════════╣
+\033[1;32m║➢ Author   : HDT - TOOL                                          ║
+\033[1;36m║➢ Youtube  : https://youtube.com/@HDT-TOOL                       ║
+\033[1;31m║➣ Nhóm Zalo  : https://zalo.me/g/bprmyn080                       ║
+\033[1;33m║➣ Website  : https://hdt-tool.blogspot.com                       ║
+\033[1;32m║➣ Bot Telegram : https://t.me/hdttool                            ║
+\033[1;34m╚═════════════════════════════════════════════════════════════════╝
+\033[1;34m⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦
+"""
+ for X in banner:
+  sys.stdout.write(X)
+  sys.stdout.flush() 
+  sleep(0.000001)
+  #time key
+now = datetime.now()
+thu = now.strftime("%A")
+ngay_hom_nay = now.strftime("%d")
+thang_nay = now.strftime("%m")
+nam_ = now.strftime("%Y")
+ #--------------------KEY---------------&&#
+os.system('clear')
+banner()
+a=now.strftime("%d")
+h=int(now.strftime("%d"))
+ngay_trc=h-2
+ngay_mai=h+1
+time=datetime.now().strftime("%Hh:%Mp:%Ss")
+b=now.strftime("%m")
+day=now.strftime("%d-%m-%Y")
+today=now.strftime("%d-%m-%Y")
+hientai=now.strftime("%m-%Y")
+ngay=int(strftime('%d'))
+key1=str(ngay*199872767+168299)
+key='HDT-'+key1
+long_url=(f"https://html-css-js.hdt-tool.repl.co/?key={key}")
+#dilink nhìn tự hiểu nha chứ ko hiểu t chịu
+url_dilink = requests.get(f'https://dilink.net/QL_api.php?token=3eedacbad8de78dafc867ef2d8a29e1a3e1c942b4d9b9b9533216590dc9c585c&url={long_url}').text
+##print(url_dilink)
+if '<link rel="canonical" href="' in url_dilink:
+    link_key_dilink = url_dilink.split('<link rel="canonical" href="')[1].split('">')[0]
+else:
+    link_key_dilink = 'Không Thể Tạo Url!!!'
+#print(link_key_dilink)
+file_key=f'key_ngay{ngay_hom_nay}.txt'
+file_key_cu=f'key_ngay{ngay_trc}.txt'
+check_file_key=os.path.exists(file_key)
+if check_file_key == False:
+   print(f"{thanh_xau}{vang}Đây Là Tool Free Nên Key Sẽ Thay Đổi Mỗi Ngày !!")   
+   print(f'{thanh_xau}{vang}Truy Cập Vào Link{trang}: {link_key_dilink} {vang}')
+   print(f'{trang}-----------------------------------------------------------------')
+   while(True):
+      ma=input(f"{thanh_xau}{vang}Nhập API Key\033[1;32m Ngày \033[1;37m{today}: \033[1;33m")
+      if ma == key:
+         print(f'{thanh_xau}{vang}API Key Chính Xác')
+         luu=open(file_key, 'a+')
+         luu.write(ma)
+         luu.close()
+         break
+      elif ma != key:
+         print(f'{thanh_xau}{do}API Key Sai')
+elif check_file_key == True:
+    print(f'{thanh_xau}{vang}Đang Lấy Key...',end='\r')
+    sleep(1)
+    k=open(file_key, 'r')
+    ma=k.read()
+    k.close()
+    if ma == key:
+        print(f'{thanh_xau}{vang}Lấy Key Thành Công       ',end='\r')
+        sleep(0.5)
+    elif ma != key:
+        if os.path.exists(file_key) == True:
+            os.system(f'rm {file_key}')
+        os.system(f'rm {file_key}')
+        print(f'{thanh_xau}{do}API Key Sai         ')
+        while(True):
+            ma=input(f"{thanh_xau}{vang}Nhập API Key\033[1;32m Ngày \033[1;37m{today}: \033[1;33m")
+            if ma == key:
+                print(f'{thanh_xau}{vang}API Key Chính Xác')
+                luu=open(file_key, 'a+')
+                luu.write(ma)
+                luu.close()
+                break
+            elif ma != key:
+                print(f'{thanh_xau}{do}API Key Sai           ')
+import os, sys, requests
+from time import sleep
+from pystyle import *
+from time import strftime
+from datetime import datetime, timedelta
+now=datetime.now()
+os.system("cls" if os.name == "nt" else "clear")
+banner="""
+\033[1;34m╔═════════════════════════════════════════════════════════════════╗
+\033[1;32m║ ██╗  ██╗██████╗ ████████╗  ████████╗ ██████╗  ██████╗ ██╗       ║
+\033[1;35m║ ██║  ██║██╔══██╗╚══██╔══╝  ╚══██╔══╝██╔═══██╗██╔═══██╗██║       ║
+\033[1;31m║ ███████║██║  ██║   ██║ █████╗ ██║   ██║   ██║██║   ██║██║       ║
+\033[1;33m║ ██╔══██║██║  ██║   ██║ ╚════╝ ██║   ██║   ██║██║   ██║██║       ║
+\033[1;34m║ ██║  ██║██████╔╝   ██║        ██║   ╚██████╔╝╚██████╔╝███████╗  ║
+\033[1;37m║ ╚═╝  ╚═╝╚═════╝    ╚═╝        ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝  ║
+\033[1;34m╠═════════════════════════════════════════════════════════════════╣
+\033[1;32m║➢ Author   : HDT - TOOL                                          ║
+\033[1;36m║➢ Youtube  : https://youtube.com/@HDT-TOOL                       ║
+\033[1;31m║➣ Nhóm Zalo  : https://zalo.me/g/bprmyn080                       ║
+\033[1;33m║➣ Website  : https://hdt-tool.blogspot.com                       ║
+\033[1;32m║➣ Nhóm Telegram : https://t.me/hdttool                           ║
+\033[1;34m╚═════════════════════════════════════════════════════════════════╝
+"""
+for X in banner:
+  sys.stdout.write(X)
+  sys.stdout.flush() 
+  sleep(0.00)
+print("\033[1;34m⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦")
+print("\033[1;33m╔═════════════════════╗")
+print("\033[1;33m║ \033[1;34mTOOL Trao Đổi Sub \033[1;33m  ║ ")
+print("\033[1;33m╚═════════════════════╝")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[1] \033[1;32mTDS TIKTOK + TIKTOK NOW")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[2] \033[1;32mTDS BẰNG PAGE PRO5 [1]")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[3] \033[1;32mTDS FACEBOOK FULL JOD")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[4] \033[1;32mTDS BẰNG PAGE PRO5 Đa Luồng [2]")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[5] \033[1;32mTDS INSTAGRAM MAX SPEED")
+print("\033[1;33m╔═════════════════════╗ ")
+print("\033[1;33m║ \033[1;34mTOOL Tương Tác Chéo \033[1;33m║ ")
+print("\033[1;33m╚═════════════════════╝ ")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[6] \033[1;32mTTC PAGE PRO5 [1]")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[7] \033[1;32mTTC INSTAGRAM VIPIG")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[8] \033[1;32mTTC PAGE PRO5 [2] ")
+print("\033[1;33m╔═════════════════════╗ ")
+print("\033[1;33m║ \033[1;34mTOOL TIỆN ÍCH PRO5 \033[1;33m ║ ")
+print("\033[1;33m╚═════════════════════╝ ")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[9] \033[1;32mBUFF CẢM XÚC STORY BẰNG PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[10] \033[1;32mBUFF VIEW STORY BẰNG PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[11] \033[1;32mKÍCH HOẠT PAGE PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[12] \033[1;32mBUFF FOLOW BẰNG PAGE PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[13] \033[1;32mCHUYỂN QUYỀN QTV PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[14] \033[1;32mBUFF TV NHÓM BẰNG PAGE PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[15] \033[1;32mREG PAGE PRO5 + ÚP ĐẠI DIỆN")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[16] \033[1;32mSHARE ẢO BẰNG PRO5 [TOKEN]")
+print("\033[1;33m╔══════════════════════╗ ")
+print("\033[1;33m║ \033[1;34mTOOL ENCODE + DECODE \033[1;33m║ ")
+print("\033[1;33m╚══════════════════════╝ ")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[17] \033[1;32mENCODE - Độc Quyền HDT-TOOL")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[18] \033[1;32mENCODE - INPOSTOR")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[19] \033[1;32mENCODE - 5 LỚP")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[20] \033[1;32mENCODE - MARSHAL 16 CHẾ ĐỘ")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[21] \033[1;32mENCODE - Emoji 4 Chế Độ")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[22] \033[1;32mENCODE - MARSHAL")
+print("\033[1;33m╔══════════════════════╗ ")
+print("\033[1;33m║ \033[1;34mTOOL TIỆN ÍCH KHÁC   \033[1;33m║ ")
+print("\033[1;33m╚══════════════════════╝ ")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[23] \033[1;32mKẾT BẠN FACEBOOK GỢI Ý")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[24] \033[1;32mNUÔI NICK FACEBOOK")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[25] \033[1;32mGET TOKEN FACEBOOK")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[26] \033[1;32mTOOL LỌC PROXY")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[27] \033[1;32mTOOL TẤN CÔNG WEBSITE \033[1;33m[ NEW ]")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[28] \033[1;32mTOOL NUÔI NICK FACEBOOK")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[30] \033[1;32mTOOL SHARE ẢO PRO5 [ MAX SPEED ]")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[31] \033[1;32mREG ACC GARENA")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[32] \033[1;32mGET TOKEN PAGE PRO5")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[33] \033[1;32mTOOL BUFF VIEW TIKTOK \033[1;31m[ Chưa Dùng Được ]")
+print("\033[1;31m ▶ Nhập Số \033[1;36m[34] \033[1;32mTOOL BUFF CMT BÀI VIẾT FB \033[1;33m[ NEW ]")
+print("\033[1;34m⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦")
+chon = int(input('\033[1;31m ▶ Nhập Số \033[1;32m: \033[1;33m'))
+if chon == 1 :
+	exec(requests.get('https://run.mocky.io/v3/8221ea33-c46a-45e1-bb2c-94b057cd89a5').text)
+if chon == 2 :
+	exec(requests.get('https://run.mocky.io/v3/c3abc6b6-5e02-42de-9b3b-f9496b97dd00').text)
+if chon == 3 :
+	exec(requests.get('https://run.mocky.io/v3/f7a6abc2-1d8e-4c3c-9f77-88e3f6f73c91').text)
+if chon == 4 :
+	exec(requests.get('https://run.mocky.io/v3/d398d205-ee9e-41c4-957e-9897b910fe3f').text)
+if chon == 5 :
+ exec(requests.get('https://run.mocky.io/v3/33ac725c-ee55-48c3-8624-c30f943e3abf').text)
+if chon == 6 :
+	exec(requests.get('https://run.mocky.io/v3/570fc93b-7e82-468a-b81c-8177e370d486').text)
+if chon == 7 :
+	exec(requests.get('https://run.mocky.io/v3/b09d1365-c10c-4c6a-8d01-ae53a9666c1a').text)
+if chon == 8 :
+	exec(requests.get('https://run.mocky.io/v3/11ab0ee0-2345-4ef4-bc7e-11c253bb5292').text)
+if chon == 9 :
+	exec(requests.get('https://run.mocky.io/v3/6ed8107c-f203-490d-868a-6c7e6a6a9c13').text)
+if chon == 10 :
+	exec(requests.get('https://run.mocky.io/v3/36486b40-c74d-4891-bf67-d853cf2b406e').text)
+if chon == 11 :
+	exec(requests.get('https://run.mocky.io/v3/3464ef7c-c98d-4ac9-acce-4081983794d2').text)
+if chon == 12 :
+	exec(requests.get('https://run.mocky.io/v3/48b4f5ad-02a1-4428-a1d8-0642bc1074dc').text)
+if chon == 13 :
+	exec(requests.get('https://run.mocky.io/v3/a6314cc6-c342-4cc0-ab76-93c4d160140e').text)
+if chon == 14 :
+	exec(requests.get('https://run.mocky.io/v3/58ff6704-4fd4-46b2-aa7d-cd3f647b47e9').text)
+if chon == 15 :
+ exec(requests.get('https://run.mocky.io/v3/c75f2097-e07c-4688-95ed-e5b4377cfbb4').text)
+if chon == 16 :
+ exec(requests.get('https://run.mocky.io/v3/f848f336-d6f1-4866-bea2-d99315330770').text)
+if chon == 17 :
+ exec(requests.get('https://run.mocky.io/v3/97bc5d55-1f06-4e6a-8866-189d294a7971').text)
+if chon == 18 :
+ exec(requests.get('https://run.mocky.io/v3/8a72c023-da9a-4c30-95de-61dcce1c9da3').text)
+if chon == 19 :
+ exec(requests.get('https://run.mocky.io/v3/dc04fadb-0fdd-4b94-be59-a04363a63335').text)
+if chon == 20 :
+ exec(requests.get('https://run.mocky.io/v3/429a3b2d-8762-41da-b139-fdd4f7a544b6').text)
+if chon == 21 :
+ exec(requests.get('https://run.mocky.io/v3/f8c726ba-ed27-41c6-b66d-3c51a118774b').text)
+if chon == 22 :
+ exec(requests.get('https://run.mocky.io/v3/02ca6814-2b44-4e3e-af4e-02a38d5f3a82').text)
+if chon == 23 :
+ exec(requests.get('https://run.mocky.io/v3/f4b73973-0727-4636-8bfe-8e06ee18017a').text)
+if chon == 24 :
+ exec(requests.get('https://run.mocky.io/v3/f3b075a8-4983-40a6-b104-56cab87f6dfb').text)
+if chon == 25 :
+ exec(requests.get('https://run.mocky.io/v3/ab9c3051-3e49-4404-9517-a86031dcf738').text)
+if chon == 26 :
+ exec(requests.get('https://run.mocky.io/v3/82fdd44e-cae1-45fc-a00c-93a4f6b1f98c').text)
+if chon == 27 :
+ exec(requests.get('https://run.mocky.io/v3/e3c77030-fcc1-44f0-b22f-99f9364c2518').text)
+if chon == 28 :
+ exec(requests.get('https://run.mocky.io/v3/1dd805d4-adea-4324-a55f-3660bb83ff63').text)
+if chon == 29 :
+ exec(requests.get('chưa').text)
+if chon == 30 :
+ exec(requests.get('https://run.mocky.io/v3/f844c62e-39b5-49a1-a54c-f01dc48c6c3d').text)
+if chon == 31 :
+ exec(requests.get('https://run.mocky.io/v3/1d2fe0c2-9e4b-4125-b6dd-ff400baf052c').text)
+if chon == 32 :
+ exec(requests.get('https://run.mocky.io/v3/883422bb-81a1-4b72-9d67-99a4ef73ddbb').text)
+if chon == 33 :
+ exec(requests.get('https://run.mocky.io/v3/cdd22615-4233-47bc-bdc9-e84b5c874c52').text)
+if chon == 34 :
+ exec(requests.get('https://run.mocky.io/v3/e804bfc9-d96e-49a7-a0ae-7989d99a3919').text)
+if chon == 35 :
+ exec(requests.get('chưa').text)
+if chon == 36 :
+ exec(requests.get('https://run.mocky.io/v3/cdd22615-4233-47bc-bdc9-e84b5c874c52').text)
+if chon == 37 :
+ exec(requests.get('https://run.mocky.io/v3/e804bfc9-d96e-49a7-a0ae-7989d99a3919').text)
+else :
+	print (" Sai Lựa Chọn ")
+	exit()
